61e35f93e913aa7a1d64917d75e4518ccf436fed
saneag <garstea2002@mail.ru>
2025-11-09 21:09:42 +0200

    refactor: extract next round button logic to a hook

--- components/game-table/next-round-button/index.tsx
+++ components/game-table/next-round-button/index.tsx
@@ -1,81 +1,28 @@
-import { useCallback, useState } from 'react';
+import { View } from 'react-native';
 import { Button } from 'react-native-paper';
-import { validateEnteredScore } from '../../../helpers/gameScoreValidationHelpers';
-import { useGameStore } from '../../../store/game';
-import { usePlayersStore } from '../../../store/players';
+import { useHandleNextRound } from '../../../hooks/useHandleNextRound';
 import ConfirmationDialog from '../../confirmation-dialog';
 import DialogContent from './dialogContent';
 import DialogTitle from './dialogTitle';
 
 export default function NextRoundButton() {
-  const [isEmptyGame, setIsEmptyGame] = useState(false);
-  const [isScoreValid, setIsScoreValid] = useState(true);
-
-  const playersCount = usePlayersStore((state) => state.playersCount);
-  const isTeamVsTeam = usePlayersStore((state) => state.isTeamVsTeam);
-  const dealer = usePlayersStore((state) => state.dealer);
-  const setDealer = usePlayersStore((state) => state.setDealer);
-
-  const score = useGameStore((state) => state.score);
-  const setNextScore = useGameStore((state) => state.setNextScore);
-  const currentRound = useGameStore((state) => state.currentRound);
-
-  const handleNextRound = useCallback(
-    (showDialog: VoidFunction) => {
-      const validation = validateEnteredScore({
-        roundScore: score[currentRound],
-        playersCount,
-        setIsEmptyGame,
-        isTeamVsTeam,
-      });
-
-      if (validation.isEmptyGame) {
-        showDialog();
-        return;
-      }
-
-      if (!validation.isValid) {
-        setIsScoreValid(false);
-        showDialog();
-        return;
-      }
-
-      setIsScoreValid(true);
-      setNextScore(playersCount);
-      setDealer(dealer + 1 === playersCount ? 0 : dealer + 1);
-    },
-    [
-      currentRound,
-      dealer,
-      isTeamVsTeam,
-      playersCount,
-      score,
-      setDealer,
-      setNextScore,
-    ]
-  );
-
-  const handleAddEmptyRow = useCallback(() => {
-    if (isEmptyGame) {
-      setNextScore(playersCount);
-    }
-
-    setDealer(dealer + 1 === playersCount ? 0 : dealer + 1);
-  }, [dealer, isEmptyGame, playersCount, setDealer, setNextScore]);
+  const { handleNextRound, isScoreValid } = useHandleNextRound();
 
   return (
-    <ConfirmationDialog
-      title={<DialogTitle isEmptyGame={isEmptyGame} />}
-      content={<DialogContent isEmptyGame={isEmptyGame} />}
-      renderShowDialog={(showDialog) => (
-        <Button
-          mode='contained'
-          onPress={() => handleNextRound(showDialog)}>
-          Next round
-        </Button>
-      )}
-      isConfirmationButtonVisible={isScoreValid}
-      confirmationCallback={handleAddEmptyRow}
-    />
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+      <ConfirmationDialog
+        title={<DialogTitle />}
+        content={<DialogContent />}
+        renderShowDialog={(showDialog) => (
+          <Button
+            mode='contained'
+            onPress={showDialog}>
+            Next round
+          </Button>
+        )}
+        isConfirmationButtonVisible={isScoreValid}
+        confirmationCallback={handleNextRound}
+      />
+    </View>
   );
 }
