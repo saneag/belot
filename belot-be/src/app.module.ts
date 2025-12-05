ea20c9936edda6f7ce1cb8adda5953c5f30bfbee
saneag <garstea2002@mail.ru>
2025-12-05 12:52:40 +0200

    chore: add mongodb connection setup

--- belot-be/src/app.module.ts
+++ belot-be/src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import env from './config/env';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [env],
       isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('mongoUri'),
+      }),
     }),
   ],
   controllers: [],
--- belot-be/src/config/env.ts
+++ belot-be/src/config/env.ts
@@ -1,3 +1,4 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 5000,
+  mongoUri: process.env.MONGO_URI,
 });
