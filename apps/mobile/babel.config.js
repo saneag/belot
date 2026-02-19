427a342ef59dd22491b3f781f59814e5ee9fe62a
saneag <garstea2002@mail.ru>
2026-02-19 18:14:39 +0200

    fix: app build showing an error

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,7 +2,12 @@ module.exports = function (api) {
   api.cache(true);
 
   return {
-    presets: [["babel-preset-expo"], "nativewind/babel"],
+    presets: [[
+      "babel-preset-expo",
+      {
+        "react-compiler": false,
+      },
+    ], "nativewind/babel"],
 
     plugins: [
       [
--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -4,20 +4,19 @@ const { withNativeWind } = require("nativewind/metro");
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, "../..");
-const fastDevEnabled = process.env.EXPO_FAST_DEV === "1";
 
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = fastDevEnabled
-  ? [path.join(workspaceRoot, "packages"), path.join(workspaceRoot, "shared")]
-  : [workspaceRoot, path.join(workspaceRoot, ".pnpm")];
+config.watchFolders = [workspaceRoot];
 config.resolver.nodeModulesPaths = [
   path.join(projectRoot, "node_modules"),
   path.join(workspaceRoot, "node_modules"),
 ];
-if (fastDevEnabled) {
-  config.resolver.disableHierarchicalLookup = true;
-}
+config.resolver.disableHierarchicalLookup = true;
 config.resolver.unstable_enableSymlinks = true;
+config.resolver.extraNodeModules = {
+  react: path.join(projectRoot, "node_modules", "react"),
+  "react-native": path.join(projectRoot, "node_modules", "react-native"),
+};
 
 module.exports = withNativeWind(config, { input: "./styles/global.css" });
