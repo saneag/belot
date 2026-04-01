bdd4a6a73147e7f4e29a4dde9bc98d98d36d938b
saneag <garstea2002@mail.ru>
2026-04-01 11:16:43 +0300

    chore: add vitest configs for all projects

---
+++ packages/utils/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineProject, mergeConfig } from "vitest/config";
+import shared from "@belot/vitest-config";
+
+export default mergeConfig(
+  shared,
+  defineProject({
+    test: {
+      name: "@belot/utils",
+    },
+  }),
+);
