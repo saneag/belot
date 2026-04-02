3b4bd714d85884150b1ae13376eb75a123678303
saneag <garstea2002@mail.ru>
2026-04-02 11:08:58 +0300

    wip

---
+++ packages/utils/src/gameUtils/playersScoreCalculationHelpers.test.ts
@@ -0,0 +1,23 @@
+import { beforeEach, describe, it, vi } from "vitest";
+
+import { calculatePlayersScoresHelper } from "./playersScoreCalculationHelpers";
+
+describe("playersScoreCalculationHelpers", () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  describe("calculatePlayersScoresHelper", () => {
+    it("should return the calculated players scores", () => {
+      const result = calculatePlayersScoresHelper();
+    });
+  });
+
+  describe("calculatePlayersScores", () => {
+    it("should", () => {});
+  });
+
+  describe("sumOpponentPlayersScores", () => {
+    it("should", () => {});
+  });
+});
