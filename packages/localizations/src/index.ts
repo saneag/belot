9542765ceacf06b00b31852a305f7cda8995cf58
saneag <garstea2002@mail.ru>
2026-04-23 18:39:13 +0300

    feat: add server offline localization for mobile

--- packages/localizations/src/index.ts
+++ packages/localizations/src/index.ts
@@ -1,6 +1,20 @@
 import en from "./en.json";
+import { errors } from "./errors";
 import ro from "./ro.json";
 import ru from "./ru.json";
 
-export const Localizations = { en, ro, ru };
-export type LocalizationKey = keyof typeof en;
+export const Localizations = {
+  en: {
+    ...en,
+    ...errors.en,
+  },
+  ro: {
+    ...ro,
+    ...errors.ro,
+  },
+  ru: {
+    ...ru,
+    ...errors.ru,
+  },
+};
+export type LocalizationKey = keyof typeof Localizations.en;
