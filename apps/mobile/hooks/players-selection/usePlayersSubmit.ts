9f0b7355caac38b0098fffcd776697c4b05be841
saneag <garstea2002@mail.ru>
2026-04-23 20:07:47 +0300

    chore: temporary remove init game fetch

--- apps/mobile/hooks/players-selection/usePlayersSubmit.ts
+++ apps/mobile/hooks/players-selection/usePlayersSubmit.ts
@@ -1,38 +1,21 @@
 import { useCallback } from "react";
 
-import { ToastAndroid } from "react-native";
-
 import { useRouter } from "expo-router";
 
-import { useGameInit } from "@belot/api-client";
 import { StorageKeys } from "@belot/constants";
+import { usePlayersSelectionContext } from "@belot/hooks";
 import { useGameStore } from "@belot/store";
-import { GameMode } from "@belot/types";
-import { isPlayerNameValid, prepareTeams, validatePlayersNames } from "@belot/utils/src";
+import { isPlayerNameValid, validatePlayersNames } from "@belot/utils/src";
 
-import { usePlayersSelectionContext } from "@/components/players-selection/playersSelectionContext";
-
-import { getApiBaseUrl } from "@/helpers/apiBaseUrl";
 import { setMultipleItemsToStorage } from "@/helpers/storageHelpers";
-import { useLocalizations } from "@/localizations/useLocalization";
 
 export default function usePlayersSubmit() {
   const router = useRouter();
 
-  const messages = useLocalizations([
-    {
-      key: "server.offline",
-    },
-  ]);
-
   const { setValidations } = usePlayersSelectionContext();
 
-  const initGame = useGameInit(getApiBaseUrl());
-
   const players = useGameStore((state) => state.players);
-  const dealer = useGameStore((state) => state.dealer);
   const setEmptyRoundScore = useGameStore((state) => state.setEmptyRoundScore);
-  const setGameId = useGameStore((state) => state.setGameId);
 
   const handleOpenDialog = useCallback(
     (showDialog: VoidFunction) => {
@@ -63,26 +46,10 @@ export default function usePlayersSubmit() {
       [StorageKeys.hasPreviousGame]: true,
     });
 
-    const mode = players.length === 4 ? GameMode.teams : GameMode.classic;
-
-    try {
-      const gameInitResponse = await initGame.mutateAsync({
-        players,
-        mode,
-        teams: prepareTeams(players, mode),
-        dealer: dealer || null,
-      });
-
-      setGameId(gameInitResponse.id);
-    } catch (error) {
-      console.error("Error in usePlayersSubmit", error);
-      ToastAndroid.show(messages.serverOffline, ToastAndroid.SHORT);
-    }
-
     setEmptyRoundScore();
 
     router.navigate("/game-table");
-  }, [dealer, initGame, messages, players, router, setEmptyRoundScore, setGameId, setValidations]);
+  }, [players, router, setEmptyRoundScore, setValidations]);
 
   return {
     handleOpenDialog,
