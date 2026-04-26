247d8bc96bcf12f584b96b08c074aa3f7a2959c3
saneag <garstea2002@mail.ru>
2026-04-26 17:11:06 +0300

    refactor: merge use players submit hook

--- apps/mobile/components/players-selection/actionButtons.tsx
+++ apps/mobile/components/players-selection/actionButtons.tsx
@@ -1,10 +1,15 @@
-import { useHandlePlayersSelectionResetButton } from "@belot/hooks";
+import { ToastAndroid } from "react-native";
+
+import { useRouter } from "expo-router";
+
+import { useHandlePlayersSelectionResetButton, usePlayersSubmit } from "@belot/hooks";
 
 import ConfirmationDialog from "@/components/confirmationDialog";
 import { Button, ButtonText } from "@/components/ui/button";
 import { HStack } from "@/components/ui/hstack";
 
-import usePlayersSubmit from "@/hooks/players-selection/usePlayersSubmit";
+import { getApiBaseUrl } from "@/helpers/apiBaseUrl";
+import { setMultipleItemsToStorage } from "@/helpers/storageHelpers";
 import { useLocalization, useLocalizations } from "@/localizations/useLocalization";
 
 import DealerSelectDialogContent from "./dealerSelectDialogContent";
@@ -22,12 +27,24 @@ function ResetButton() {
 }
 
 function SubmitButton() {
+  const router = useRouter();
+
   const messages = useLocalizations([
     { key: "players.submit.dialog.title" },
     { key: "players.submit.dialog.button" },
+    { key: "server.offline" },
   ]);
 
-  const { handleOpenDialog, handleSubmit } = usePlayersSubmit();
+  const { handleOpenDialog, handleSubmit } = usePlayersSubmit({
+    navigateFunction: () => router.navigate("/game-table"),
+    setItemsToStorage: async (items) => {
+      await setMultipleItemsToStorage(items);
+    },
+    getApiBaseUrl,
+    handleCatchError: () => {
+      ToastAndroid.showWithGravity(messages.serverOffline, ToastAndroid.SHORT, ToastAndroid.CENTER);
+    },
+  });
 
   return (
     <ConfirmationDialog
