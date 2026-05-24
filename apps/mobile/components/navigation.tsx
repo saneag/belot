63a776c696023c67ce614d06777a625b8f199228
saneag <garstea2002@mail.ru>
2026-05-24 16:43:56 +0300

    feat: add starting screen and navigation setup for continue and new game

--- apps/mobile/components/navigation.tsx
+++ apps/mobile/components/navigation.tsx
@@ -2,6 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import GameTableScreen from "@/app/game-table";
 import PlayersSelectionScreen from "@/app/players-selection";
+import StartingScreen from "@/app/starting-screen";
 import { routes } from "@/constants/routes";
 
 const Stack = createStackNavigator();
@@ -9,12 +10,13 @@ const Stack = createStackNavigator();
 export default function Navigation() {
   return (
     <Stack.Navigator
-      initialRouteName={routes.playersSelection}
+      initialRouteName={routes.startingScreen}
       screenOptions={{
         headerShown: false,
         cardStyle: { backgroundColor: "transparent", padding: 0, margin: 0 },
       }}
     >
+      <Stack.Screen name={routes.startingScreen} component={StartingScreen} />
       <Stack.Screen name={routes.playersSelection} component={PlayersSelectionScreen} />
       <Stack.Screen name={routes.gameTable} component={GameTableScreen} />
     </Stack.Navigator>
