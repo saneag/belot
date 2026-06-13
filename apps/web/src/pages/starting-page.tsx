d9a707bc2d5e5b6e837dbffc31d94bd763aafb7e
saneag <garstea2002@mail.ru>
2026-06-13 22:06:55 +0300

    perf: update starting screen with logo and title

--- apps/web/src/pages/starting-page.tsx
+++ apps/web/src/pages/starting-page.tsx
@@ -7,6 +7,15 @@ export default function StartingPage() {
 
   return (
     <div className="flex h-full items-center justify-center">
+      <div className="absolute top-1/6 flex flex-col items-center justify-center gap-3">
+        <img
+          src="/images/ic_launcher_no_bg.png"
+          alt="Belot-score logo"
+          width="100px"
+          height="100px"
+        />
+        <h1 className="text-center text-4xl font-normal">Belot-score</h1>
+      </div>
       <div className="flex flex-col gap-3">
         {actions.map((action) => (
           <Button key={action.index} onClick={action.onPress}>
