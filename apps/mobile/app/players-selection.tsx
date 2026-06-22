0405f8af0e1ff87097ced4d511d889f66f439717
saneag <garstea2002@mail.ru>
2026-06-22 19:48:59 +0300

    feat: add maxScore selector to players selection screen

--- apps/mobile/app/players-selection.tsx
+++ apps/mobile/app/players-selection.tsx
@@ -1,17 +1,21 @@
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import { PlayersSelectionContextProvider } from "@belot/components";
+import { useFeatureToggle } from "@belot/hooks";
 
 import { BackButton } from "@/components/backButton";
 import DismissKeyboardView from "@/components/dismissKeyboardView";
 import ActionButtons from "@/components/players-selection/actionButtons";
 import LoadPreviousGameButton from "@/components/players-selection/loadPreviousGameButton";
+import MaxScoreSelector from "@/components/players-selection/maxScoreSelector";
 import PlayersCount from "@/components/players-selection/playersCount";
 import PlayersNames from "@/components/players-selection/playersNames";
 import { Center } from "@/components/ui/center";
 import { VStack } from "@/components/ui/vstack";
 
 export default function PlayersSelectionScreen() {
+  const isMaxScoreSelectorEnabled = useFeatureToggle("max-score-selector");
+
   return (
     <KeyboardAwareScrollView
       className="flex-1 bg-phone-screen-background"
@@ -25,6 +29,7 @@ export default function PlayersSelectionScreen() {
           <VStack className="gap-3">
             <PlayersSelectionContextProvider>
               <PlayersCount />
+              {isMaxScoreSelectorEnabled && <MaxScoreSelector />}
               <PlayersNames />
               <ActionButtons />
             </PlayersSelectionContextProvider>
